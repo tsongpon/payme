fc0b303e5d1c1cd619e154d9a62e1baf5a12fe8e
Songpon <tum@LM000C6PVWHYYCT.local>
2024-08-24 08:32:49 +0700

    support input target and amount when create qr

--- internal/qr/qr.go
+++ internal/qr/qr.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"image/png"
 	"os"
+	"regexp"
 	"strconv"
 
 	"github.com/yousifnimah/Cryptx/CRC16"
@@ -78,25 +79,34 @@ func CalChecksum(data string) string {
 	Input := []byte(data)          //string to slice of bytes
 	AlgorithmName := "CCITT_FALSE" //CRC-8 algorithm name from supported table
 	checksumHex := CRC16.ResultHex(Input, AlgorithmName)
-	// fmt.Println("Output:", checksumHex)
 	return checksumHex
 }
 
-func CreatePrompPayQR() string {
-	formatedPromptPayPhoneNumber := "0066809710099"
+func CreatePrompPayQR(target string, amount float64) string {
 	payloadFormatIndicator := qrPromptPayField{id: "00", name: "Payload Format Indicator", length: 2, value: "01"}
-	poiMethod := qrPromptPayField{id: "01", length: 2, name: "Point of Initiation Method", value: "11"}
+
+	var poiMethod qrPromptPayField
+	if amount == 0.0 {
+		poiMethod = qrPromptPayField{id: "01", length: 2, name: "Point of Initiation Method", value: "11"}
+	} else {
+		poiMethod = qrPromptPayField{id: "01", length: 2, name: "Point of Initiation Method", value: "12"}
+	}
 
 	applicationIDSubField := qrPromptPayField{id: "00", name: "Application ID", length: 16, value: "A000000677010111"}
-	promptPayPhoneNumberSubField := qrPromptPayField{id: "01", name: "Prompt phone number", length: 13, value: formatedPromptPayPhoneNumber}
+	promptPayPhoneNumberSubField := qrPromptPayField{id: "01", name: "Prompt phone number", length: 13, value: formatTraget(target)}
 	merchantAccountInformation := qrPromptPayField{id: "29", length: 37, name: "Marchant Account Information",
 		subField: []*qrPromptPayField{&applicationIDSubField, &promptPayPhoneNumberSubField}}
 
 	thbISOCurrencyCode := "764"
 	currency := qrPromptPayField{id: "53", name: "Transaction Currency", length: 3, value: thbISOCurrencyCode}
+	var transactionAmt qrPromptPayField
+	if amount != 0.0 {
+		amountVal := formatAmount(amount)
+		transactionAmt = qrPromptPayField{id: "54", name: "Transcation amount", length: len(amountVal), value: amountVal}
+	}
 	country := qrPromptPayField{id: "58", name: "Country Code", length: 2, value: "TH"}
 
-	qr := serialize(payloadFormatIndicator) + serialize(poiMethod) + serialize(merchantAccountInformation) + serialize(country) + serialize(currency)
+	qr := serialize(payloadFormatIndicator) + serialize(poiMethod) + serialize(merchantAccountInformation) + serialize(transactionAmt) + serialize(country) + serialize(currency)
 	checksum := qrPromptPayField{id: "63", name: "CRC 16 Chcksum", length: 4}
 	qr = qr + serialize(checksum)
 	qr = qr + CalChecksum(qr)[2:6]
@@ -105,17 +115,22 @@ func CreatePrompPayQR() string {
 }
 
 func serialize(field qrPromptPayField) string {
-	var output string
+	if field.id == "" {
+		return ""
+	}
 	if field.subField == nil {
-		output = field.id + formatFieldLengthStr(field.length) + field.value
+		return field.id + formatFieldLengthStr(field.length) + field.value
 	} else {
 		var subFieldOutput string
 		for _, sub := range field.subField {
-			subFieldOutput = subFieldOutput + sub.id + formatFieldLengthStr(sub.length) + sub.value
+			subFieldOutput = subFieldOutput + serialize(*sub)
 		}
-		output = field.id + formatFieldLengthStr(field.length) + subFieldOutput
+		return field.id + formatFieldLengthStr(field.length) + subFieldOutput
 	}
-	return output
+}
+
+func formatAmount(amount float64) string {
+	return strconv.FormatFloat(amount, 'f', 2, 64)
 }
 
 func formatFieldLengthStr(l int) string {
@@ -126,4 +141,17 @@ func formatFieldLengthStr(l int) string {
 		lengthStr = strconv.Itoa(l)
 	}
 	return lengthStr
+}
+
+func formatTraget(target string) string {
+	allNumCheck := regexp.MustCompile(`^[0-9]`)
+	if allNumCheck.MatchString(target) {
+		if len(target) == 10 { // is phone number
+			return "0066" + target[1:]
+		}
+		if len(target) == 13 { // is citizen number
+			return target
+		}
+	}
+	return ""
 }
--- internal/qr/qr_test.go
+++ internal/qr/qr_test.go
@@ -2,41 +2,47 @@ package qr_test
 
 import (
 	"fmt"
+	"image/png"
+	"os"
 	"testing"
 
 	"github.com/tsongpon/payme/internal/qr"
 )
 
-// func TestCreateQRCode(t *testing.T) {
-// 	qrCode := qr.CreateQRCode("0899999999", 20)
-// 	img := qr.CraeteQRImage(qrCode)
-// 	//save the imgByte to file
-// 	out, err := os.Create("./QRImg666.png")
+func TestCreateQRCode(t *testing.T) {
+	qrCode := qr.CreateQRCode("0899999999", 20)
+	img := qr.CraeteQRImage(qrCode)
+	//save the imgByte to file
+	out, err := os.Create("./QRImg666.png")
 
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-// 	err = png.Encode(out, img)
+	err = png.Encode(out, img)
 
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
-// 	if qrCode != "00020101021229370016A000000677010111011300668999999995802TH5303764540520.0063044DAF" {
-// 		t.Error("qr.Create(\"0899999999\", \"20\") should return 00020101021229370016A000000677010111011300668999999995802TH5303764540520.0063044DAF, but got ", qrCode)
-// 	}
-// }
-
-func TestChecksum(t *testing.T) {
-	crc := qr.CalChecksum("00020101021129370016A000000677010111011300660000000005802TH53037646304")
-	if crc != "0x8956" {
-		t.Error("checksum should return 0x8956, but got ", crc)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if qrCode != "00020101021229370016A000000677010111011300668999999995802TH5303764540520.0063044DAF" {
+		t.Error("qr.Create(\"0899999999\", \"20\") should return 00020101021229370016A000000677010111011300668999999995802TH5303764540520.0063044DAF, but got ", qrCode)
 	}
 }
 
-func TestCreatePrompPayQR(t *testing.T) {
-	qr := qr.CreatePrompPayQR()
-	fmt.Println(qr)
+func TestCreatePrompPayQRValidPhoneNumber(t *testing.T) {
+	expected := "00020101021129370016A000000677010111011300660000000005802TH530376463048956"
+	qr := qr.CreatePrompPayQR("0000000000", 0)
+	if qr != "00020101021129370016A000000677010111011300660000000005802TH530376463048956" {
+		t.Error("CreatePrompPayQR should return " + expected + ", but got " + qr)
+	}
+}
+
+func TestCreatePrompPayQRValidCitizenNumber(t *testing.T) {
+	expected := "00020101021129370016A000000677010111011331305551294625802TH530376463045858"
+	qr := qr.CreatePrompPayQR("3130555129462", 0)
+	if qr != "00020101021129370016A000000677010111011331305551294625802TH530376463045858" {
+		t.Error("CreatePrompPayQR(\"3130555129462\", 0) should return " + expected + ", but got " + qr)
+	}
 }
